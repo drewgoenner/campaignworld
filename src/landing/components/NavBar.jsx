acf92e4d1763eb2f542de5137dc49f70e9ea394a
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-05-12 15:44:23 -0500

    made navbar look more like a navbar

--- src/landing/components/NavBar.jsx
+++ src/landing/components/NavBar.jsx
@@ -3,31 +3,112 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Link, Typography, ButtonBase } from '@material-ui/core';
 
 
-const useStyles = makeStyles(theme => ({
-   
-
-}));
-
 export default function Nav() {
     const classes = useStyles();
 
     return (
-        <Typography >
-            <div>
-                
-                Andrew Goenner
+        <Typography className = {classes.wrapper}>
+            <div className = {classes.left}>
+                <h2>CWC</h2>
             </div>
-            <div >
-                <a href = '#' >Home</a>
-                <a href = '#register' >Register</a>
-                <a href = '#login'>Login</a>
-            </div>
-            <div>
+            <div className = {classes.middle}>
+                <h1>Campaign World Creator</h1>
                 
-                <h2>Computers & Coders</h2>
+            </div>
+            <div className = {classes.right}>
+                <a href = '#' className = {classes.tabs} >Home</a>
+                <a href = '#register' className = {classes.tabs} >Register</a>
+                <a href = '#login' className = {classes.tabs}>Login</a>
             </div>
         </Typography>
     )
 
     
 }
+
+
+//below here all material-ui styling for the page
+const useStyles = makeStyles(theme => ({
+   
+    wrapper: {
+
+        padding: theme.spacing(1),
+        [theme.breakpoints.down('sm')]: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            margin: '0',
+            width: '100%'
+            
+        },
+        [theme.breakpoints.up('md')]: {
+            
+        },
+        [theme.breakpoints.up('lg')]: {
+            
+        },
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '99%',
+        margin: '10px',
+        borderBottom: '2px solid green'
+    },
+
+    link: {
+        fontSize: '16px',
+        padding: '10px'
+    },
+
+    image: {
+        width: '100px'
+    },
+
+    img: {
+        display: 'flex',
+        margin: '0',
+        width: '70%',
+    },
+
+    left: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+
+    },
+
+    right: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'space-between',
+    },
+
+    middle: {
+        padding: theme.spacing(1),
+        [theme.breakpoints.down('sm')]: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+
+            
+        },
+        [theme.breakpoints.up('md')]: {
+            
+        },
+        [theme.breakpoints.up('lg')]: {
+            
+        },
+        display: 'flex',
+        alignItems: 'flex-end'
+    },
+
+    tabs: {
+        margin: '0 10px',
+        textDecoration: 'none',
+        color: 'white',
+       
+        
+    }
+
+}));
